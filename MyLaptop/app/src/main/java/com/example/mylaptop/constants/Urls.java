84037d8d1e916fe22e8958caabac3a591315bb5e
DEV <dev@step.rivne>
2024-11-11 18:08:26 +0200

    Android

--- MyLaptop/app/src/main/java/com/example/mylaptop/constants/Urls.java
+++
@@ -1,5 +0,0 @@
-package com.example.mylaptop.constants
-
-public class Urls{
-    public static final String BASE = "http://10.0.2.2:5020";
-}
--- laptop/utils/createBaseQuery.ts
+++
@@ -1,7 +0,0 @@
-import { fetchBaseQuery } from '@reduxjs/toolkit/query'
-import { BASE_URL } from '@/constants/Urls'
-
-export const createBaseQuery = (endpoint: string) =>
-    fetchBaseQuery({
-        baseUrl: `${BASE_URL}/api/${endpoint}/`,
-    })
